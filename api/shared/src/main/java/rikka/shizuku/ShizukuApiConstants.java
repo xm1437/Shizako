646d3c8bd4f991d41ae0a9553588b26070b9719c
shizako-port <shizako@local>
2026-08-16 05:26:48 +0000

    release zako2.2: new private signing key
    - Replaced the AOSP public testkey with a dedicated RSA-4096 key
      (SHA256withRSA, valid until 2056). Note: signature change, users of
      zako2.1 need to uninstall before installing this build.
    - Version bump: zako2.1 -> zako2.2 (SERVER_PATCH_VERSION 2, api 2.2.0)

--- api/shared/src/main/java/rikka/shizuku/ShizukuApiConstants.java
+++ api/shared/src/main/java/rikka/shizuku/ShizukuApiConstants.java
@@ -7,7 +7,7 @@ import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP_PREFIX;
 public class ShizukuApiConstants {
 
     public static final int SERVER_VERSION = 2;
-    public static final int SERVER_PATCH_VERSION = 1;
+    public static final int SERVER_PATCH_VERSION = 2;
 
     // binder
     public static final String BINDER_DESCRIPTOR = "moe.shizuku.server.IShizukuService";
